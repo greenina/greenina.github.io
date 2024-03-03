0a6ecc8615c92a4297e789c150386ef585e5f461
greenina <igreen0485@gmail.com>
2024-03-04 02:21:12 +0900

    Add kaist href

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
           <section id="about">
             <div>My research interests are centered around the dynamic interplay between technology design and its influence on individual well-being and mental health. I am particularly fascinated by the ways in which people engage with language models and personal informatics systems. Additionally, I believe that creativity has the power to be a catalyst for enhancing mental health and well-being, driving me to explore the nexus of technology and creativity, supporting individuals to seek innovative ways in each of their unique contexts to actively improve well-being and mental health. </div>
             <div className='middle'>
-              I am a highly motivated undergraduate HCI researcher at <a>
+              I am a highly motivated undergraduate HCI researcher at <a href="https://cs.kaist.ac.kr/">
                   KAIST School of Computing
                 </a>, starting a research intern at 
                 <a>NAVER AI Lab</a>, 
