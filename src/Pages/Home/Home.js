d90892303742e24c7b7f13490e3d69d7f4cbeb45
greenina <igreen0485@gmail.com>
2024-05-14 02:22:19 -1000

    Customize text style

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -24,13 +24,13 @@ const Home = () => {
       </section>
       <section class="right">
         <section id="about">
-          <div>My research interests are centered around the dynamic interplay between technology design and its influence on individual well-being and mental health. I am particularly fascinated by the ways in which people engage with language models and personal informatics systems. Additionally, I believe that creativity has the power to be a catalyst for enhancing mental health and well-being, driving me to explore the nexus of technology and creativity, supporting individuals to seek innovative ways in each of their unique contexts to actively improve well-being and mental health. </div>
+          <div>My recent research interests are centered around the dynamic interplay between technology design and its influence on individual well-being and mental health. I am particularly fascinated by the ways in which people engage with <b>language models</b> and <b>personal informatics</b> systems. Additionally, I believe that <b>creativity</b> has the power to be a catalyst for enhancing <b>mental health</b> and <b>well-being</b>, driving me to explore the nexus of technology and creativity, supporting individuals to seek innovative ways in each of their unique contexts to actively improve well-being and mental health. </div>
           <div className='middle'>
           These interests guide my work as a highly motivated undergraduate HCI researcher at <a href="https://cs.kaist.ac.kr">
                 KAIST School of Computing
               </a>, currently a research intern at <a>NAVER AI Lab</a>, where I am fortunate to be
-              mentored by <a href="http://younghokim.net">Dr. Young-Ho Kim</a>, also to work with <a href="http://munmund.net">Dr. Munmun De Choudhury</a>. 
-              I was also incredibly lucky to have worked with  <a href="https://galaxytourist.notion.site/galaxytourist/Hwajung-Hong-cc10b0291bbe4ca38dbf4882cd687423">Dr. Hwajung Hong</a>, <a href="https://jangheecho15.github.io">Dr. Janghee Cho</a>, and <a href="http://juhokim.com">Dr. Juho Kim</a>, who inspired my love and devotion to HCI research💚.   
+              mentored by <a href="http://younghokim.net" target="_blank">Dr. Young-Ho Kim</a>, also to work with <a href="http://munmund.net" target="_blank">Dr. Munmun De Choudhury</a>. 
+              I was also incredibly lucky to have worked with  <a href="https://galaxytourist.notion.site/galaxytourist/Hwajung-Hong-cc10b0291bbe4ca38dbf4882cd687423" target="_blank">Dr. Hwajung Hong</a>, <a href="https://jangheecho15.github.io" target="_blank">Dr. Janghee Cho</a>, and <a href="http://juhokim.com" target="_blank">Dr. Juho Kim</a>, who all inspired my love and devotion to HCI research💚.   
           </div>
           <p className='style1'>I am a Ph.D. applicant for Fall 2025!</p>
         </section>
