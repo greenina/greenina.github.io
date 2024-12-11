89137e5f1e9cc2dcbddba50934e5d9c1a3a76337
greenina <igreen0485@gmail.com>
2024-12-11 15:15:50 +0900

    Acknowledgements

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -4,7 +4,7 @@ export const Footer = (props) => {
     return (
         <div className='bg-primary text-secondary text-left py-10 pl-10'>
             <div>Copyright © 2023-2024 Inhwa Song. All Rights Reserved. The website was designed by Inhwa Song and built with React.js.</div>
-            <div>The design was inspired by multiple other wonderful personal websites (<span className='italic'>incl.</span> <a href="http://younghokim.net/" target="_blank">[1]</a>, <a href="https://haesookim.info/" target="_blank">[2]</a>, <a href="https://daeunchoi.com/" target="_blank">[3]</a>).</div>
+            <div>Acknowledgements: The design of this website was inspired by multiple other wonderful personal websites (<span className='italic'>incl.</span> <a href="http://younghokim.net/" target="_blank">[1]</a>, <a href="https://haesookim.info/" target="_blank">[2]</a>, <a href="https://daeunchoi.com/" target="_blank">[3]</a>).</div>
         </div>
     )
 }
