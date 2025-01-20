6deb52615aa836d19fb33a5bb24d64b0d97c2ed0
greenina <igreen0485@gmail.com>
2025-01-20 15:24:34 +0900

    Update news and publication

--- src/Components/NewsEntry/NewsEntry.js
+++ src/Components/NewsEntry/NewsEntry.js
@@ -1,6 +1,6 @@
 const NewsEntry = ({ news }) => {
   return (
-    <div className='flex flex-row gap-3 text-normal'>
+    <div className='flex flex-row gap-3 text-normal py-1'>
       <div className='font-semibold'>{news.date}</div>
       <div className=''>{news.content}</div>
     </div>
