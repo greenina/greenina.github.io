248743c413eb9b53b6154d54fabacb91d90b91ab
greenina <igreen0485@gmail.com>
2025-05-10 13:28:11 +0900

    update X id

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -20,7 +20,7 @@ const Home = () => {
               className="w-12 h-12 transition-transform duration-200 hover:scale-110"
             />
           </a>
-          <a href="https://twitter.com/inhwa_song_" target="_blank">
+          <a href="https://twitter.com/inhwa_april" target="_blank">
             <img
               src="/icons/twitter_x.svg"
               alt="twitter"
